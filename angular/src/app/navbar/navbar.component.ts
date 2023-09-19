c33272e1e4880612812df06bceded0255686b91f
Ekim <ekimbasoglu@hotmail.com>
2023-09-19 19:06:23 +0200

    Navbar updated

--- angular/src/app/navbar/navbar.component.ts
+++ angular/src/app/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/authService';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  hideComponent: boolean = true;
+  constructor(private router: Router, private authService: AuthService) {}
 
+  ngOnInit(): void {
+    // Check if it's the dashboard page
+    // and if the user has credentials
+    if (this.router.url === '/dashboard') {
+      // TODO: Check if the data recieved has plant is actually dry
+      this.hideComponent = false;
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
