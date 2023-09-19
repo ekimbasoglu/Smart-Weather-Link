d2ed324d7da3a8eca13976599491760d4741071e
Ekim <ekimbasoglu@hotmail.com>
2023-09-19 16:01:59 +0200

    Notes added, API updated
    Notes has all the basic instructions
    API updated in a way to read data from Serial communication which is coming from Arduino Uno

--- api/server.js
+++ api/server.js
@@ -1,17 +1,44 @@
 const SerialPort = require("serialport").SerialPort;
-// import { SerialPort } from 'serialport';
+const Readline = require("@serialport/parser-readline");
+const { ReadlineParser } = require('@serialport/parser-readline')
+const express = require("express");
+const cors = require('cors');
+const app = express();
 
-// Replace "/dev/cu.usbserial-10" with your specific serial port name
-const port = new SerialPort({ path: "/dev/cu.usbserial-10", baudRate: 57600 })
-
-port.write('main screen turn on', function (err) {
-    if (err) {
-        return console.log('Error on write: ', err.message)
-    }
-    console.log('message written')
-})
+// Define the serial port settings
+const serialPort = new SerialPort({
+    path: "/dev/cu.usbserial-10",
+    baudRate: 9600, // Adjust to match your Arduino's baud rate
+});
+const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
 // Open errors will be emitted as an error event
-port.on('error', function (err) {
+serialPort.on('error', function (err) {
     console.log('Error: ', err.message)
 })
+
+
+// Cors
+app.use(cors());
+
+
+// Define the GET endpoint
+app.get("/read-serial-data", (req, res) => {
+    // Read data from the serial port
+    parser.once("data", (data) => {
+        console.log("Received data from serial port:", data);
+        res.send(data); // Send the received data as the HTTP response
+    });
+});
+
+// Start the HTTP server
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
+
+// port.write('main screen turn on', function (err) {
+//     if (err) {
+//         return console.log('Error on write: ', err.message)
+//     }
+//     console.log('message written')
+// })
---
+++ notes.md
@@ -0,0 +1,22 @@
+## TO-DO
+For now it makes transaction with server.js, just writing
+I uploaded necesarry arduino code to the device, so;
+
+### [server.js] - needs to read what are the outputs
+
+### [server.js] - it needs to send those outputs through open PORT; so FE can read
+
+### [update on arduino] - it needs to make those transaction based on bluetooth
+
+### [FE] - FE needs to fetch those data through Node.js
+### [FE] - If the plant is dry based on sensor, there must be button which as option so user can interract with additional sensor
+### [FE] - [BE] - Communication must be instant, maybe with Socket is required.
+
+
+## Expected Serial communication;
+
+
+Humidity on Air (%): 49.00
+Temperature (°C): 26.60
+Soil is dry
+
