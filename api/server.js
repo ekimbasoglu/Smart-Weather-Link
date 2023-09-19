10fde2a799378acc99cc333d6867e07c01d94a38
Ekim <ekimbasoglu@hotmail.com>
2023-09-19 17:33:20 +0200

    Socket.io added, updated with reading the data and emitting through

--- api/server.js
+++ api/server.js
@@ -4,10 +4,19 @@ const { ReadlineParser } = require('@serialport/parser-readline')
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const io = require('socket.io')(3001);
+
+// Socket connection
+io.on('connection', (socket) => {
+    console.log('Connection has been established with browser.');
+    socket.on('disconnect', () => {
+        console.log('Browser client disconnected from the connection.');
+    });
+});
 
 // Define the serial port settings
 const serialPort = new SerialPort({
-    path: "/dev/cu.usbserial-10",
+    path: "/dev/cu.usbserial-110",
     baudRate: 9600, // Adjust to match your Arduino's baud rate
 });
 const parser = serialPort.pipe(new ReadlineParser({ delimiter: '\r\n' }))
@@ -17,17 +26,48 @@ serialPort.on('error', function (err) {
     console.log('Error: ', err.message)
 })
 
+// Initialize some data (for demonstration purposes)
+let dataFromArduino = {
+    temperature: '',
+    humidtyIndoor: '',
+    humidtyPlant: ''
+};
 
 // Cors
 app.use(cors());
 
+// Socket.io event handling
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-// Define the GET endpoint
-app.get("/read-serial-data", (req, res) => {
+    // Send initial data to the connected client
     // Read data from the serial port
-    parser.once("data", (data) => {
-        console.log("Received data from serial port:", data);
-        res.send(data); // Send the received data as the HTTP response
+    parser.on("data", (data) => {
+        // Generate a switch that if the data starts with certain text then it will be assigned to a variable
+        switch (true) {
+            case data.startsWith("Humidity on Air"):
+                dataFromArduino.humidtyIndoor = data.replace("Humidity on Air: ", "");
+                break;
+            case data.startsWith("Temperature"):
+                dataFromArduino.temperature = data.replace("Temperature: ", "");
+                break;
+            case data.startsWith("Soil is dr"):
+                dataFromArduino.humidtyOnPlant = data.replace("Humidity on plant: ", "");
+                break;
+            case data.startsWith("Soil is we"):
+                dataFromArduino.humidtyOnPlant = data.replace("Humidity on plant: ", "");
+                break;
+            default:
+                break;
+        }
+
+        io.emit("data", dataFromArduino);
+        console.log(dataFromArduino);
+    });
+
+    // Handle disconnect event
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
     });
 });
 
